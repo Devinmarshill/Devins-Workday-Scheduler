812308f7cdab70a578b6277d0e2b5b2b6689f377
Devin <devinhill2911@gmail.com>
2023-07-19 16:19:52 -0400

    function and for loop

--- script.js
+++ script.js
@@ -2,8 +2,19 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
+
+var currentDayEl=$("#currentDay")
+var currentDayTime=dayjs().format("dddd MMMM, DD")
+console.log(currentDayTime)
+currentDayEl.text('')
+var currentHour= dayjs().hour()
+console.log(currentHour)
+
+  for (let i = 9; i < 18; i++) {
+    
+    
+  }
+// TODO: Add a listener for click events on the save button. This code shoudata/ use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
